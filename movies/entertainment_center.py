09ffbaeb9598d5f105bc679a647c0f9fe0fe5109
harindersingh <im.harinder.singh@gmail.com>
2015-10-25 12:34:28 +0530

    Project:Movie website v1.0

---
+++ movies/entertainment_center.py
@@ -0,0 +1,15 @@
+import media
+
+toy_story = media.Movie("Toy Story",
+						"A story of a boy and his toys that come to life",
+						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+print(toy_story.storyline)
+
+avatar = media.Movie("Avatar",
+					"A marine on an alien planet",
+					"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+					"https://www.youtube.com/watch?v=5PSNL1qE6VY")
+
+print(avatar.storyline)
