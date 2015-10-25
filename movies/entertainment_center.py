98edecef542c30a1ad3d575c032f9fa88406270f
harindersingh <im.harinder.singh@gmail.com>
2015-10-25 13:03:07 +0530

    Project: Movie Website v2.0

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -1,15 +1,40 @@
 import media
+import fresh_tomatoes
 
 toy_story = media.Movie("Toy Story",
 						"A story of a boy and his toys that come to life",
 						"http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
 						"https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-print(toy_story.storyline)
+#print(toy_story.storyline)
 
 avatar = media.Movie("Avatar",
 					"A marine on an alien planet",
 					"https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
 					"https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-print(avatar.storyline)
+#print(avatar.storyline)
+#avatar.show_trailer()
+
+school_of_rock = media.Movie("School of Rock",
+					"Using rock music to learn",
+					"https://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+					"https://www.youtube.com/watch?v=3PsUJFEBC74")
+
+ratatouille = media.Movie("Ratatouille",
+					"A rat is a chef in Paris",
+					"http://ia.media-imdb.com/images/M/MV5BMTMzODU0NTkxMF5BMl5BanBnXkFtZTcwMjQ4MzMzMw@@._V1_SX214_AL_.jpg",
+					"https://www.youtube.com/watch?v=c3sBBRxDAqk")
+
+midnight_in_Paris = media.Movie("Midnight in Paris",
+					"Going back in time to meet at authors",
+					"https://upload.wikimedia.org/wikipedia/en/thumb/9/9f/Midnight_in_Paris_Poster.jpg/215px-Midnight_in_Paris_Poster.jpg",
+					"https://www.youtube.com/watch?v=5nOF93SzX6s")
+
+hunger_games = media.Movie("Hunger Games",
+					"A really real reality show",
+					"http://www.apnatimepass.com/the-hunger-games-catching-fire-movie-poster-1.jpg",
+					"https://www.youtube.com/watch?v=mfmrPu43DF8")
+
+movies = [toy_story, avatar, school_of_rock, ratatouille, midnight_in_Paris, hunger_games]
+fresh_tomatoes.open_movies_page(movies)
--- movies/media.py
+++ movies/media.py
@@ -1,3 +1,5 @@
+import webbrowser
+
 class Movie():
 	def __init__(self, movie_title, movie_storyline, poster_image,
 	 				trailer_youtube):
@@ -5,3 +7,6 @@ class Movie():
 		self.storyline = movie_storyline
 		self.poster_image_url = poster_image
 		self.trailer_youtube_url = trailer_youtube
+
+	def show_trailer(self):
+		webbrowser.open(self.trailer_youtube_url)
